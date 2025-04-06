54e7c2fd3c12cb45d9528560e3565ee0d875f3ee
Ahmad Taimur <ullahtaimur@gmail.com>
2025-04-06 23:50:32 +0500

    commit

--- script.js
+++ script.js
@@ -19,5 +19,36 @@ document.getElementById('uploadForm').addEventListener('submit', function (e) {
       console.error(error);
     });
 });
+function loadFileList() {
+  fetch('/uploaded-files')
+    .then(response => response.json())
+    .then(files => {
+      const fileList = document.getElementById('fileList');
+      fileList.innerHTML = '';
+      files.forEach(file => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+          <a href="/download/${file.filename}" target="_blank">${file.filename}</a>
+          <button class="btn download-btn" onclick="downloadFile('${file.filename}')">Download</button>
+          `;
+        fileList.appendChild(li);
+      });
+  })
+  .catch(error => {
+      console.error('Error fetching file list:', error);
+  });
+}
+
+
+function downloadFile(filename) {
+  window.location.href = `/download/${filename}`;
+}
+
+document.getElementById('fileInput').addEventListener('change', function (e) {
+  const fileName = e.target.files[0].name;
+  document.querySelector('.upload-area p').textContent = `Selected: ${fileName}`;
+});
+
+loadFileList();
 
   
